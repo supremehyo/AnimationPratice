db67d3655b42f8878014290e8309d7db34ba67d4
supremehyo <36808015+supremehyo@users.noreply.github.com>
2021-09-02 11:07:53 +0900

    어떤 상태인지에 따라서 여러 애니메이션을 하도록 기능추가

--- app/src/main/java/com/supremehyo/animationpratice/MainActivity.kt
+++ app/src/main/java/com/supremehyo/animationpratice/MainActivity.kt
@@ -3,6 +3,7 @@ package com.supremehyo.animationpratice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,11 +11,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        motionLayout.setOnClickListener {
-            Log.v("sdfsdf" ,"ssssssssssss")
-            motionLayout2.velocity
-            motionLayout2.transitionToEnd()
-           // motionLayout2.transitionToStart()
+    }
+
+    fun nextState(view: View) {
+        when (motionlayout.currentState ) {
+            R.id.state1 -> motionlayout.transitionToState(R.id.state2)
+            R.id.state2 -> motionlayout.transitionToState(R.id.state3)
+            R.id.state3 -> motionlayout.transitionToState(R.id.state4)
+            R.id.state4 -> motionlayout.transitionToState(R.id.state5)
+            R.id.state5 -> motionlayout.transitionToState(R.id.state1)
         }
     }
 }
